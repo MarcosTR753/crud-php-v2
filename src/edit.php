d0ff193f91a8277c63c659bfb1fe8992f10bab28
Marcos <marcos.tejidorodriguez@iesmiguelherrero.com>
2024-06-18 08:48:39 +0200

    modifico

--- src/edit.php
+++ src/edit.php
@@ -83,6 +83,7 @@ mysqli_stmt_close($stmt);
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<title>Modificación Coches</title>
 <!--	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
